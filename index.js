ba02cb931f48cded546c35f2da64c1442fb49f93
Jakhongir Mirzakhmatov <94097414+Jakhongir-M@users.noreply.github.com>
2022-05-19 05:24:48 +0500

    arifmeticss

---
+++ index.js
@@ -0,0 +1,33 @@
+alert(" arifmetik msalalarga hush kelibsiz");
+
+alert("siz qoshadigan sonni kiriting");
+
+let q = +prompt("qoshmoqchi bolgan sonni kiriting");
+let w = +prompt("ikkinchi sonni kiriting");
+
+let plus = q + w;
+alert(plus);
+
+alert("siz kopaytirish arifmetik masaldasiz");
+let e = +prompt("kopaytiriladigan  sonni kiriting");
+let r = +prompt("ikkinchi sonni kiriting");
+
+let multiplication = e * r;
+
+alert(multiplication);
+
+alert("siz bolish arifmetik masaldasiz");
+let t = +prompt("bo'ladigan sonni kiriting");
+let y = +prompt("ikkinchi sonni kiriting");
+
+let division = t / y;
+
+alert(division);
+
+alert("siz ayirish arifmetik masaldasiz");
+let u = +prompt("ayriladigan sonni kiriting");
+let i = +prompt("ikkinchi sonni kiriting");
+
+let minus = u - i;
+
+alert(minus);
